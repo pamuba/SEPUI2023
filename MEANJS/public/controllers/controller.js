8af025a92fd71019f0cbd84052d1774028d3fb23
kavyajadav <kjadhav@stevens.edu>
2023-11-21 02:08:37 +0530

    mean2

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -1,24 +1,22 @@
-function AppCtrl(){
+function AppCtrl($scope, $http){
     console.log("Hello World from Controller")
 
-    person1 = {
-        name:"John",
-        email: "john@gmail.com",
-        number:"(111) 111-1111"
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            $scope.contactlist = response;
+            $scope.contact = ""
+        });
     };
-    person2 = {
-        name:"Jack",
-        email: "jack@gmail.com",
-        number:"(222) 222-222"
-    };
-    person1 = {
-        name:"Jill",
-        email: "jill@gmail.com",
-        number:"(333) 333-3333"
-    };
-    person1 = {
-        name:"Adma",
-        email: "adam@gmail.com",
-        number:"(444) 444-4444"
-    };
+    
+    refresh();
+
+
+    $scope.addContacts = function(){
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            console.log(response)
+            refresh();
+        })
+    }
+
+
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -1,12 +1,25 @@
 var express = require('express');
-
 var app = express();
-
-// app.get('/', function(req, res){
-//     res.send("Hello World from server.js");
-// });
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser')
+var db = mongojs("mongodb://localhost:27017/contactlist", ["contactlist"]);
 
 app.use(express.static(__dirname+"/public"))
+app.use(bodyParser.json())
+
+
+app.get('/contactlist', function(req, res){
+    db.contactlist.find(function(err, docs){
+        res.json(docs)
+    })
+});
+
+
+app.post('/contactlist', function(req, res){
+   db.contactlist.insert(req.body, function(err, docs){
+    res.json(docs)
+   })
+})
 
 app.listen(3000);
 console.log("Server running at PORT:3000")
