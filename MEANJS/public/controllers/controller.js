a6321302108df53b1181a44252b4e9eb4653504f
kavyajadav <kjadhav@stevens.edu>
2023-11-22 02:58:18 +0530

    meanDone

--- MEANJS/public/controllers/controller.js
+++ MEANJS/public/controllers/controller.js
@@ -18,5 +18,23 @@ function AppCtrl($scope, $http){
         })
     }
 
+    $scope.remove = function(id){
+        $http.delete('/contactlist/' + id).success(function(response){
+            refresh();
+        })
+    }
+
+    $scope.edit = function(id){
+        $http.get('/contactlist/' + id).success(function(response){
+            $scope.contact = response;
+        })
+    }
+
+    $scope.update = function(){
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+        })
+    }
+
 
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -21,5 +21,30 @@ app.post('/contactlist', function(req, res){
    })
 })
 
+app.delete('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc)
+    })
+})
+
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc)
+    })
+})
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    db.contactlist.findAndModify({
+        query: {_id: mongojs.ObjectId(id)},
+        update: {$set:{name:req.body.name, email:req.body.email, number:req.body.number}},
+        new: true}, function(err, doc){
+            res.json(doc)
+    })
+})
+
 app.listen(3000);
 console.log("Server running at PORT:3000")
