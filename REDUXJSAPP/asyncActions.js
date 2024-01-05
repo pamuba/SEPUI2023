06cac0acf45c9a9770db2aef719862fa8c7898b7
kavyajadav <kjadhav@stevens.edu>
2024-01-06 02:57:16 +0530

    redux2

---
+++ REDUXJSAPP/asyncActions.js
@@ -0,0 +1,85 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require('redux-logger');
+const axios = require('axios');
+const thunkMiddleware = require('redux-thunk').default;
+
+const logger = reduxLogger.createLogger();
+
+
+
+const initialState = {
+    loading: false,
+    user:[],
+    error:''
+}
+
+//actions
+const FETCH_USERS_REQUESTS = 'FETCH_USERS_REQUESTS'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUESTS
+    }
+}
+const fetchUsersSuccess = (users) => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+const fetchUsersFailure = (error) => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
+//Reducer
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_USERS_REQUESTS:
+         return {
+            ...state,
+            loading: true
+        }
+        case FETCH_USERS_SUCCESS:
+         return {
+            ...state,
+            loading: false,
+            users: action.payload,
+            error: ''
+        }
+        case FETCH_USERS_FAILURE:
+         return {
+            ...state,
+            loading: false,
+            users:[],
+            error: error
+        }
+        default: return state
+    }
+}
+
+//async action
+const fetchUsers = () => {
+    return function(dispatch){
+        dispatch(fetchUsersRequest()) //sync action
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                const users = response.data
+                dispatch(fetchUsersSuccess(users)) //sync actions
+            })
+            .catch(error => {
+                dispatch(fetchUsersFailure(error)) //sync actions
+            })
+    }
+}
+
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(()=>{})
+store.dispatch(fetchUsers())
+//unsubscribe()
--- REDUXJSAPP/index.js
+++ REDUXJSAPP/index.js
@@ -1,5 +1,10 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require('redux-logger');
+
+const logger = reduxLogger.createLogger();
 
 const BUY_LAPTOP = 'BUY_LAPTOP'
 const BUY_PRINTERS = 'BUY_PRINTERS'
@@ -21,25 +26,33 @@ function buyPrinters(){
 
 //Reducer
 // (prevState, action) => newState
-const initialState = {
-    numOfLaptops:10,
+const initialLaptopState = {
+    numOfLaptops:10
+}
+
+const initialPrinterState = {
     numOfPrinters:30
 }
 
-const reducer = (state=initialState, action) => {
+const laptopReducer = (state=initialLaptopState, action) => {
     switch(action.type){
         case BUY_LAPTOP : return {
             ...state,
             numOfLaptops: state.numOfLaptops - 1
         }
+        default: return state
+    }
+}
+const printerReducer = (state = initialPrinterState, action) => {
+    switch(action.type){
         case BUY_PRINTERS : return {
             ...state,
             numOfPrinters: state.numOfPrinters - 1
         }
         default: return state
+
     }
 }
-
 //holds app state
 //allows access to state via getState()
 //allows state mutation vis dispatch(action)
@@ -47,13 +60,19 @@ const reducer = (state=initialState, action) => {
 //Unregisters listeners vis the fn returned by subscribe(listener)
 
 //store
-const store = createStore(reducer)
+
+const rootReducer = combineReducers({
+    laptops:laptopReducer,
+    printers:printerReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial State:', store.getState())
 
 
 //()=> console.log('Update State:', store.getState()) == LISTENER
 //LISTENER runs on every state change
-const unsubscribe = store.subscribe(()=> console.log('Update State:', store.getState()))
+const unsubscribe = store.subscribe(()=> {})
 
 //onCLick of a button we dispatch a action
 store.dispatch(buyLaptop())
